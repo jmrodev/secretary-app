5b9cf83a7aa933cbdcd380149f2b67a269c6c841
jmrodev <juanmarcelo.rodrigueztandil@gmail.com>
2026-04-18 22:32:30 -0300

    chore: limpieza definitiva de scripts obsoletos, tools innecesarios y documentacion vieja

--- deploy_pi5/duckdns/duck.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Script de actualización para Duck DNS
-# Reemplaza TU_DOMINIO y TU_TOKEN con tus datos de duckdns.org
-
-DOMINIO="TU_DOMINIO"
-TOKEN="TU_TOKEN"
-
-echo url="https://www.duckdns.org/update?domains=${DOMINIO}&token=${TOKEN}&ip=" | curl -k -o /home/pi/duckdns/duck.log -K -
