a60a784cc72efddbb9f19ff2f4b24ebc3138360e
jmrodev <juanmarcelo.rodrigueztandil@gmail.com>
2026-08-21 20:06:09 -0300

    refactor(config): remove duplicate institutions and profile tabs from system config

--- client/src/features/institutions/index.js
+++ client/src/features/institutions/index.js
@@ -3,8 +3,6 @@
 export { useInstitutionsController } from '@/features/institutions/hooks/useInstitutionsController';
 
 export { InstitutionsPage } from '@/features/institutions/InstitutionsPage';
-export { InstitutionManager } from '@/features/institutions/components/views/InstitutionManager';
 export { InstitutionFormModal } from '@/features/institutions/components/forms/InstitutionFormModal';
-export { InstitutionList } from '@/features/institutions/components/views/InstitutionList';
 export { InstitutionForm } from '@/features/institutions/components/forms/InstitutionForm';
 export { InstitutionSelector } from '@/features/institutions/components/ui/InstitutionSelector';
